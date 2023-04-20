daf10fefe659be50956fb102d75cc945c2406d57
Aditya <mp7020216@gmail.com>
2023-04-21 04:14:49 +0530

    Add header and footer components as well as react-bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Aapki Apni dukaan mei aapka swagat hai!</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Aao Ji Dukaan Vich </h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
