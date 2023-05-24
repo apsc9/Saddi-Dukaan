fc2ae7ff5e5c27a206a842f2263130cdf6ca72ff
Aditya <mp7020216@gmail.com>
2023-05-24 16:51:56 +0530

    Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,14 +7,16 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Aadi's Bakes</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'>
+            Aadi's Bakes
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link as={Link} to='/cart'>
                 <i className='fas fa-shopping-cart'></i>Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link as={Link} to='/login'>
                 <i className='fas fa-user'></i>Sign In
               </Nav.Link>
             </Nav>
